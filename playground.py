ea7c1673f52006e216229b4ac1ec250f97bfeca5
Ryan Guide <ryan.guide@comcast.net>
2021-05-27 17:03:12 -0400

    Added delete method

--- playground.py
+++ playground.py
@@ -2,16 +2,13 @@ import main as tree
 
 
 
-# node = tree.Node(89)
-# tree.insert(root=node, key=12)
-# tree.insert(root=node, key=11)
-# tree.insert(root=node, key=100)
-# tree.insert(root=node, key=50)
-# tree.print_values(root=node)
-
 node = tree.Node(89)
 tree.insert(root=node, key=12)
 tree.insert(root=node, key=11)
 tree.insert(root=node, key=100)
 tree.insert(root=node, key=50)
-tree.search_tree(node, 50)
+tree.print_values(root=node)
+tree.delete_node(node, key=89)
+tree.print_values(root=node)
+
+
